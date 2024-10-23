9c4cea2aa823e128a8be041a6db21338ad090f3d
Habib Asseiss <habibasseiss@gmail.com>
2024-10-22 20:54:32 -0400

    adiciona tailwind, shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Button } from "./components/ui/button";
+
+function Menu() {
+  return (
+    <div className="bg-purple-800 text-white">
+      Menu
+    </div>
+  );
+}
+
+function Conteudo() {
+  return (
+    <div>
+      <Button variant="default">
+        Remover
+      </Button>
+      <Button variant="outline">
+        Clique aqui agora!
+      </Button>
+    </div>
+  );
+}
+
+function Rodape() {
+  return (
+    <div>Rodape</div>
+  );
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Menu />
+      <Conteudo />
+      <Rodape />
     </>
-  )
+  );
 }
 
 export default App
